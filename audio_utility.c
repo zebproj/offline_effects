3045ccba7dc22df4db9e3ef796e468a1f6d722cc
clear <andrew@localhost.localdomain>
2013-09-27 15:33:27 +0000

    testing string corruption bug

--- audio_utility.c
+++ audio_utility.c
@@ -9,16 +9,19 @@
 #include <string.h>
 #include <samplerate.h>
 
-#define REVERSE 0
-#define INFO 1
+
+#define INFO 0
+#define REVERSE 1
+#define SAMPLE_RATE 2
 
 void info(const char *infilename, SF_INFO *sfinfo);
 void reverse(SNDFILE *infile, SNDFILE *outfile, const char *infilename, char *outfilename, SF_INFO *sfinfo);
+void sample_rate(SNDFILE *infile, SNDFILE *outfile, const char *infilename, char *outfilename, SF_INFO *sfinfo, double new_sample_rate);
 
 int main(int argc, const char **argv){
 	
 	if (argc < 3){
-		printf("USAGE: inputFileName effect\n");
+		printf("USAGE: inputFileName effect parameters(if any)\n");
 		return 1;
 	}
 
@@ -29,14 +32,20 @@ int main(int argc, const char **argv){
 	const char *infilename = argv[1];
 	const char *effect_arg = argv[2];
 	int effect = 0;
+	double new_sample_rate;
 	char outfilename[300];
+
+	if (argc > 3)
+		new_sample_rate = atoi(argv[3]);
 
 	infile = sf_open(infilename, SFM_READ, &sfinfo);
 	int len  = (int)strlen(infilename);
 	len -=4;
 	len = strxfrm(outfilename, infilename, len);
 
+	// Reverse
 	if (!(strcmp(effect_arg, "reverse"))){
+
 		effect = REVERSE;
 		if (sfinfo.format == 131074)
 			strcat(outfilename, "_Reverse.aif");
@@ -49,12 +58,28 @@ int main(int argc, const char **argv){
 
 		reverse(infile, outfile, infilename, outfilename, &sfinfo);
 
+	// Info
 	} else if (!(strcmp(effect_arg, "info"))){
 
 		effect = INFO;
 		info(infilename, &sfinfo);
 
-	} 
+	// Sample Rate
+	} else if (!(strcmp(effect_arg, "sr"))){
+		
+		effect = SAMPLE_RATE;
+		if (sfinfo.format == 131074)
+			strcat(outfilename, "_SR.aif");
+		else if (sfinfo.format == 65538 || sfinfo.format == 65539)
+			strcat(outfilename, "_SR.wav");
+		else {
+			printf("Only .wav or .aif files are allowed\n");
+			return 1;
+		}
+		
+		sample_rate(infile, outfile, infilename, outfilename, &sfinfo, new_sample_rate);
+
+	}
 
 	return 0;
 
@@ -110,4 +135,55 @@ void info(const char *infilename, SF_INFO *sfinfo){
 	return;
 }
 
+void sample_rate(SNDFILE *infile, SNDFILE *outfile, const char *infilename, char *outfilename, SF_INFO *sfinfo, double new_sample_rate){
+
+	int readcount, i = 0, frames = sfinfo->frames, error;
+	long total_frames = sfinfo->frames*sfinfo->channels;
+	SRC_DATA src_data;  
+	float *data = malloc(sizeof(float)*(frames*sfinfo->channels));
+	float *dataToWrite = malloc(sizeof(float)*(frames*sfinfo->channels));
+	const char* error_string;
+
+	
+	if (!infile)
+	{
+		printf("Not able to open input file %s.\n", infilename);
+		puts(sf_strerror (NULL));
+		return;
+	}
+
+	outfile = sf_open(outfilename, SFM_WRITE, sfinfo);
+
+	if (!outfile)
+	{
+		printf("Not able to open output file %s.\n", outfilename);
+		puts(sf_strerror(NULL));
+		return;
+	}
+
+	readcount = sf_readf_float(infile, data, frames);
+
+	src_data.data_in = data;
+	src_data.input_frames = total_frames;
+	src_data.data_out = dataToWrite;
+	src_data.output_frames = total_frames-2;
+	src_data.src_ratio = new_sample_rate/sfinfo->samplerate;
+
+	error = src_simple(&src_data, 0, sfinfo->channels);		
+
+	if (error){
+		error_string =  src_strerror(error);
+		printf("%s\n", error_string);
+	}
+
+	sf_writef_float(outfile, dataToWrite, frames);
+
+	free(data);	
+	sf_close(infile);
+	sf_close(outfile);
+
+	return;	
+
+
+}
 
